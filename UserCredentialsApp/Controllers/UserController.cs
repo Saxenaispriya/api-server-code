270b095653430794bfe3220efabf0d76ab6946de
Priyanka saxena <saxena23priya@gmail.com>
2023-05-13 16:42:12 +0530

    second commit

--- UserCredentialsApp/Controllers/UserController.cs
+++ UserCredentialsApp/Controllers/UserController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml;
 using UserCredentialsApp.Models;
 
 namespace UserCredentialsApp.Controllers
@@ -9,14 +12,21 @@ namespace UserCredentialsApp.Controllers
     {
 
         private DatabaseContext  dbContext;
+        //Guid newGuid = Guid.NewGuid();
+        
+     
 
         public UserController(DatabaseContext _dbcontext)
         {
             this.dbContext = _dbcontext;
         }
 
+        
+        
+
 
         [HttpGet]
+
         public IActionResult GetUser(string username)
         {
             var user = dbContext.userRegisters.FirstOrDefault(u => u.username == username);
@@ -26,16 +36,39 @@ namespace UserCredentialsApp.Controllers
             {
                 return NotFound();
             }
-            return Ok(user.Password);
+
+            var opt = new JsonSerializerOptions() { WriteIndented = true };
+            string strJson = JsonSerializer.Serialize(user, opt);
+
+           
+            return Ok(strJson);
 
         }
 
         [HttpPost]
         public async Task<IActionResult> AddUser([FromBody] UserRegister userreg)
         {
+            if (userreg == null)
+            {
+                return BadRequest("Invalid user registration data.");
+            }
+            // userreg.id = new Guid();
+
+            //  var usersavedata = dbContext.userRegisters.Add(userreg);
+
+            // var opt = new JsonSerializerOptions() { WriteIndented = true };
+            // string strJson = JsonSerializer.Serialize(usersavedata, opt);
+            //// dbContext.userRegisters.Add(userreg);
+            // await dbContext.SaveChangesAsync();
+            // return Ok(strJson);
+
+            userreg.id = new Guid();
             dbContext.userRegisters.Add(userreg);
             await dbContext.SaveChangesAsync();
-            return Ok();
+
+            var opt = new JsonSerializerOptions() { WriteIndented = true };
+            string strJson = JsonSerializer.Serialize(userreg, opt);
+            return Ok(strJson);
 
         }
     }
